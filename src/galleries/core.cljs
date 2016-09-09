989107697944748820a575a1daa98b814d5f3dcd
Dmitry Kolobov <dmkolobov@gmail.com>
2016-09-09 01:52:08 -0600

    Implement window dimensions.

--- src/galleries/core.cljs
+++ src/galleries/core.cljs
@@ -1,6 +1,6 @@
 (ns galleries.core
   (:require [reagent.core :as reagent]
-            [re-frame.core :refer [dispatch-sync]]
+            [re-frame.core :refer [dispatch dispatch-sync]]
             [galleries.db :as db]
             [galleries.subs :as subs]
             [galleries.events :as events]
@@ -12,6 +12,11 @@
   []
   [views/gallery])
 
-(dispatch-sync [:initialize-db])
+(dispatch-sync [:initialize-db {"foo.jpg" 0
+                                "bar.jpg" 1
+                                "car.jpg" 2}])
+
+(dispatch [:window-resized])
+
 (reagent/render-component [hello-world]
                           (. js/document (getElementById "app")))
--- src/galleries/events.cljs
+++ src/galleries/events.cljs
@@ -1,7 +1,23 @@
 (ns galleries.events
   (:require [galleries.db :refer [default-value]]
-            [re-frame.core :refer [reg-event-db]]))
+            [re-frame.core :refer [reg-event-db reg-event-fx]]))
+
+(defn window-resized-handler
+  "Get the inner width and inner height of the browser window for computing
+  gallery layout."
+  [{:keys [db]} _]
+  {:db (assoc db
+              :window
+              {:width  (.-innerWidth js/window)
+               :height (.-innerHeight js/window)})})
+
+(reg-event-fx
+  :window-resized
+  window-resized-handler)
 
 (reg-event-db
   :initialize-db
-  (fn [db _] (merge db default-value)))
+  (fn [db [_ images]]
+    (merge db
+           default-value
+           {:images images})))
